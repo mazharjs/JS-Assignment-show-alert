6a644af45a68f9a843fb2fd18d3f381d811aec3c
Mazhar Ahmed <ma9822649@gmail.com>
2024-01-11 00:15:42 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,72 @@
+// <!-- chapter 01 -->
+
+alert("Error! please enter a valid password")
+alert("welcome to js land\nhappy coding")
+alert("welcome to js land")
+alert("Happy coding")
+alert("hellow i can run js through my web browse console")
+
+// <!-- chapter 02 -->
+
+let user_name 
+let my_name = "mazhar ahmed"
+let messaga ;
+messaga = "Hello World";
+alert(message);
+var studentName = "mazhar ahmed";
+var studentAge = 18;
+var studentGrade = "A+1";
+var studentAddress = "123 Main Street, City";
+var studentContact = "mazherahmad@gmail.com";
+
+alert("Student Name: " + studentName);
+alert("Student Age: " + studentAge);
+alert("Student Grade: " + studentGrade);
+alert("Student Address: " + studentAddress);
+alert("Student Contact: " + studentContact);
+
+
+let pizza ="pizza \npizz \npiz \npi \np"
+alert(pizza);
+
+
+
+let email = "mazherahmad@gmail.com"
+alert("My Email is " + email);
+
+let book = "A smarter way to learn JavaScript "
+alert(book)
+
+document.write("Yah ! I can write HTML content through java script") 
+
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+let age = "i am 15 year old";
+alert(age)
+
+
+
+let birth_year = 2005 ;
+document.write("my birth year is " + birth_year);
+
+
+
+let visitorName = "John Doe";
+let productTitle = "T-shirt";
+let quantity = 5;
+
+
+let message = visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.";
+
+document.write(message);
+
+let array = [["banana", "mango", "apple"],["banana", "mango", "apple"],["banana", "mango", "apple"]]
+array.push("grapes"); //adding in last
+array.unshift("mazhar"); //adding in start
+ array.pop(); //removing from last
+ array.shift();//removing from first
+array [1] = "ahmed";//adding any where element not wirk in nested
+array.splice(2,0,"kiwi","lll")
+
+ 
+console.log(array );
